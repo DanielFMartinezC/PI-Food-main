d4b705516b67fd14a22746e33a9844c3436dc469
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-08-01 13:51:07 -0500

    sda

---
+++ api/src/Utils/Constants.js
@@ -0,0 +1,7 @@
+const apiKey = 'e710e5d5e8194b53aad4516e0adfd3f1';
+const complexSearch =` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}`;
+
+
+module.exports = {
+    complexSearch
+}
--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,18 +1,32 @@
 const { Router } = require('express');
 const router = Router();
-const {Recipe, Op } = require('../db')
-
+const {Recipe, Op } = require('../db');
+const axios = require('axios');
+const { complexSearch } = require('../Utils/')
 module.exports = router;
 
 router.get('/', async (req, res)=> {
     try{
         const { name } = req.query;
+        const { data } = await axios.get(complexSearch);
         const recipes = await Recipe.findAll({
             where: {
-                [Op.substring]: `%${name}%`
+                name: {
+                    [Op.substring]: `%${name}%`
+                }
             }
-        })
+        });
+        if(recipes){
+            return res.json(recipes.concat(data))
+        }else{
+            res.status(404);
+            throw  new Error('no recipe found')
+        }
     }catch(e){
         return e
     }
 });
+
+router.get('/:id', async (req, res)=>{
+
+})
