619e655f125ebeb6632ff163e7e1288c0a97b1d5
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-08-03 13:19:01 -0500

    Update LandingPage.jsx

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 export default function LandingPage(){
     return (
         <div>
-            <NavLink to='/home'>
+            <Link to='/home'>
                 <button>Welcome</button>
-            </NavLink>
+            </Link>
         <img src="https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YWVzdGhldGljJTIwZm9vZHxlbnwwfHwwfHw%3D&w=1000&q=80" alt='aesthetic food'/>
         </div>
     )
