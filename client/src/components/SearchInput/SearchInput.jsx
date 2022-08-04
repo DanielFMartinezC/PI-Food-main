cfbe3d3f59d88cf05a9b662ea1c32bc5f6febd6d
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-08-04 12:25:51 -0500

    Update SearchInput.jsx

---
+++ client/src/components/SearchInput/SearchInput.jsx
@@ -0,0 +1,22 @@
+import {useDispatch} from 'react-redux';
+import {getRecipesByName} from '../../Redux/actions';
+import React from 'react';
+
+export default function searchInput(){
+    const [state, setState] = React.useState('') 
+    const dispatch = useDispatch();
+    function handleName (e) {
+        setState(e.target.value)
+     };
+    return (
+        <div>
+            <form onSubmit={(e)=>{
+                e.preventDefault();
+                dispatch(getRecipesByName(state))
+            }}>
+                <input type="text" value={state} onChange={(e) => handleName(e)}/>
+                <button type='submit'/>
+            </form>
+        </div>
+    )
+}
