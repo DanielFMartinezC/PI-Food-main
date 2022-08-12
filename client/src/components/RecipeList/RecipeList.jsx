0ed94db95d986d9a12150898bb855d31bb371e67
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-08-12 11:31:39 -0500

    fsdfs

--- client/src/components/RecipeList/RecipeList.jsx
+++ client/src/components/RecipeList/RecipeList.jsx
@@ -10,10 +10,10 @@ export default function RecipeList() {
     const dispatch = useDispatch();
     React.useEffect(() => {
         const fn = async () => {
-            try{
-                await dispatch( getAllRecipes());
+            try {
+                await dispatch(getAllRecipes());
                 await dispatch(getDiets())
-            }catch(e){
+            } catch (e) {
                 return new Error(e)
             }
         };
@@ -33,19 +33,34 @@ export default function RecipeList() {
             setPage(page - 9)
         }
     };
+    function btnPage(e){
+        console.log(e.target.value);
+        setPage((9 * e.target.value) - 9)
+    }
     if (recipes) {
+        let numOfButtons = Math.ceil(recipes.length / 9);
+        let arr = []
+        for (let i = 1; i <= numOfButtons; i++) {
+            arr.push(i)
+        }
+
         return (
             <div>
                 <SearchInput />
                 <Filters diets={diets} />
                 <p>Let's see recipes</p>
                 <button onClick={prevPage}>Back</button>
+                {
+                    arr.map(x=>{
+                        return <button key={x} value={x} onClick={(e)=>btnPage(e)}>{x}</button>
+                    })
+                }
                 <button onClick={nextPage}>Next</button>
 
                 {
                     handlerPage().map((x) => {
-                        return <RecipeCard key={x.id} id={x.id} image={x.image} title={x.title} diets={typeof(x.diets[0]) === 'object' ? x.diets.map((x) => x.name) : x.diets}
-                         />
+                        return <RecipeCard key={x.id} id={x.id} image={x.image} title={x.title} diets={typeof (x.diets[0]) === 'object' ? x.diets.map((x) => x.name) : x.diets}
+                        />
                     })
                 }
             </div>
