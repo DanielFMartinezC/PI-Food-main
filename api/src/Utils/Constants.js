4da1ab72d317e1cb120e9f559e9b9513c986c2ea
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2022-08-17 08:23:10 -0500

    Update Constants.js

--- api/src/Utils/Constants.js
+++ api/src/Utils/Constants.js
@@ -1,9 +1,9 @@
 const {
-    apiKey,
-  } = process.env;
-  const number = 100;
-const complexSearch =` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=${number}`;
+  apiKey,
+} = process.env;
+const number = 100;
+const complexSearch = ` https://api.spoonacular.com/recipes/complexSearch?apiKey=${apiKey}&addRecipeInformation=true&number=${number}`;
 
 module.exports = {
-    complexSearch,
+  complexSearch,
 }
