28711f25b87aec581db097567ba142898ac99445
DanielFMartinezC <104692962+DanielFMartinezC@users.noreply.github.com>
2023-07-11 12:38:18 -0500

    Cambio de conexxión a Render

--- client/src/constants.js
+++ client/src/constants.js
@@ -1,4 +1,4 @@
-const URL_BASE = 'https://backfoodapp.herokuapp.com';
+const URL_BASE = 'https://backfood.onrender.com';
 const RECIPES = URL_BASE + '/recipes';
 const RECIPES_BY_NAME = RECIPES + '?name=';
 const RECIPE_DETAIL = RECIPES + '/'
